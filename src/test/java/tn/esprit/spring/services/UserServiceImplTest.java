ef1274752e18f76c4af07f9f7d06ade1d41f98d5
khaled.njim <khaled.njim@esprit.tn>
2023-11-03 00:46:44 +0100

    Added test class

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -1,10 +1,65 @@
 package tn.esprit.spring.services;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.entities.User;
+import tn.esprit.spring.repository.UserRepository;
 
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
-class UserServiceImplTest {}
+public class UserServiceImplTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testAddUser() {
+        // Créer un utilisateur fictif pour le test
+        User user = new User();
+
+        user.setLastName("Doe");
+
+        // Configurer le comportement simulé du UserRepository
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+
+        // Appeler la méthode addUser de UserServiceImpl
+        User savedUser = userService.addUser(user);
+
+        // Vérifier que l'utilisateur renvoyé n'est pas nul
+        assertNotNull(savedUser);
+
+        // Vérifier que les détails de l'utilisateur sont corrects
+
+        assertEquals("Doe", savedUser.getLastName());
+    }
+
+    // Ajoutez d'autres tests pour les autres méthodes de UserServiceImpl
+
+    @Test
+    public void testUpdateUser() {
+        // Implement your test for updateUser method
+    }
+
+    @Test
+    public void testDeleteUser() {
+        // Implement your test for deleteUser method
+    }
+
+    @Test
+    public void testRetrieveUser() {
+        // Implement your test for retrieveUser method
+    }
+}
